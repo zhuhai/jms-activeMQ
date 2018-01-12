f0feac9bd000ccf7fcd84e5c91ac12feef01d6d4
ChinaSea <hpuzhuhai@163.com>
2018-01-12 15:29:08 +0800

    '首次提交'

---
+++ src/main/java/com/zhuhai/jms/QueueProducer.java
@@ -0,0 +1,52 @@
+package com.zhuhai.jms;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 2018/1/12
+ * Time: 14:04
+ *
+ * @author: zhuhai
+ */
+public class QueueProducer {
+
+    private static final String BROKER_URL = "tcp://localhost:61616";
+    private static final String QUEUE_NAME = "queue-message";
+
+    public static void main(String[] args) throws JMSException {
+        //1.创建连接工厂
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(BROKER_URL);
+        //2.创建连接
+        Connection connection = connectionFactory.createConnection();
+        //3.开启连接
+        connection.start();
+        //4.创建会话
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //5.创建目标地址
+        Destination destination = session.createQueue(QUEUE_NAME);
+        //6.创建生产者
+        MessageProducer producer = session.createProducer(destination);
+        //7.创建消息
+        TextMessage textMessage = session.createTextMessage("Hello,Queue Message!");
+        //8.发送消息
+        producer.send(textMessage);
+        //9.释放资源
+        producer.close();
+        session.close();
+        connection.close();
+
+
+    }
+
+
+
+}
---
+++ src/main/java/com/zhuhai/jms/TopicConsumer.java
@@ -0,0 +1,55 @@
+package com.zhuhai.jms;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 2018/1/12
+ * Time: 14:24
+ *
+ * @author: zhuhai
+ */
+public class TopicConsumer {
+
+    private static final String BROKER_URL = "tcp://localhost:61616";
+    private static final String TOPIC_NAME = "topic-message";
+
+    public static void main(String[] args) throws JMSException {
+
+        //1.创建连接工厂
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(BROKER_URL);
+        //2.创建连接
+        Connection connection = connectionFactory.createConnection();
+        //3.开启连接
+        connection.start();
+        //4.创建会话
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //5.创建目标地址
+        Destination destination = session.createTopic(TOPIC_NAME);
+        //6.创建消费者
+        MessageConsumer consumer = session.createConsumer(destination);
+        //7.创建监听器
+        consumer.setMessageListener(new MessageListener() {
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println(textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+}
